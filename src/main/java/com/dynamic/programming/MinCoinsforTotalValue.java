f61dd77506e180a2e8f8f17a71bf575cd0613a69
dmahapat <debajyoti.3061@gmail.com>
2018-08-03 13:55:40 -0400

    min coin problem

--- src/main/java/com/dynamic/programming/MinCoinsforTotalValue.java
+++ src/main/java/com/dynamic/programming/MinCoinsforTotalValue.java
@@ -4,46 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MinCoinsforTotalValue {
-  /**
-   * Top down dynamic programing. Using map to store intermediate results.
-   * Returns Integer.MAX_VALUE if total cannot be formed with given coins
-   */
-  public int minimumCoinTopDown(int total, int coins[], Map<Integer, Integer> map) {
-
-    //if total is 0 then there is nothing to do. return 0.
-    if ( total == 0 ) {
-      return 0;
-    }
-
-    //if map contains the result means we calculated it before. Lets return that value.
-    if ( map.containsKey(total) ) {
-      return map.get(total);
-    }
-
-    //iterate through all coins and see which one gives best result.
-    int min = Integer.MAX_VALUE;
-    for ( int i=0; i < coins.length; i++ ) {
-      //if value of coin is greater than total we are looking for just continue.
-      if( coins[i] > total ) {
-        continue;
-      }
-      //recurse with total - coins[i] as new total
-      int val = minimumCoinTopDown(total - coins[i], coins, map);
-
-      //if val we get from picking coins[i] as first coin for current total is less
-      // than value found so far make it minimum.
-      if( val < min ) {
-        min = val;
-      }
-    }
-
-    //if min is MAX_VAL dont change it. Just result it as is. Otherwise add 1 to it.
-    min =  (min == Integer.MAX_VALUE ? min : min + 1);
-
-    //memoize the minimum for current total.
-    map.put(total, min);
-    return min;
-  }
 
   /**
    * Bottom up way of solving this problem.
@@ -93,10 +53,10 @@ public class MinCoinsforTotalValue {
     int coins[] = {7, 3, 2, 6};
     MinCoinsforTotalValue cc = new MinCoinsforTotalValue();
     Map<Integer, Integer> map = new HashMap<>();
-    int topDownValue = cc.minimumCoinTopDown(total, coins, map);
+    //int topDownValue = cc.minimumCoinTopDown(total, coins, map);
     int bottomUpValue = cc.minimumCoinBottomUp(total, coins);
 
-    System.out.print(String.format("Bottom up and top down result %s %s", bottomUpValue, topDownValue));
+    System.out.print(String.format("Bottom up and top down result %s ", bottomUpValue));
 
   }
 }
