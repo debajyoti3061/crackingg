fe3a4103078b453282c219e0b03ac0e6c1fac388
debajyoti3061 <debajyoti.3061@gmail.com>
2019-02-21 20:56:47 -0500

    new way

--- src/main/java/com/one/BitwiseOperation.java
+++ src/main/java/com/one/BitwiseOperation.java
@@ -2,6 +2,7 @@ package com.one;
 
 public class BitwiseOperation {
   public static void main(String args[]) {
+    int i=0;
     int a = 60;	/* 60 = 0011 1100 */
     int b = 13;	/* 13 = 0000 1101 */
     int c = 0;
@@ -25,7 +26,7 @@ public class BitwiseOperation {
     c = a >> 2;       /* 15 = 1111 */
     System.out.println("a >> 2  = " + c );
 
-    c = a >>> 2;      /* 15 = 0000 1111 */
+    c = a >>> 3;      /* 15 = 0000 1111 */
     System.out.println("a >>> 2 = " + c );
   }
 }
