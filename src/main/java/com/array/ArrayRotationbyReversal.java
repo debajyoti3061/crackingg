e145eb2283bf11cbd2154a4ac954b7c1918fc953
dmahapat <debajyoti.3061@gmail.com>
2019-04-26 14:01:16 -0400

    new

--- src/main/java/com/array/ArrayRotationbyReversal.java
+++ src/main/java/com/array/ArrayRotationbyReversal.java
@@ -1,6 +1,7 @@
 package com.array;
 
 /**
+ * o(n)
  * Let the array be arr[] = [1, 2, 3, 4, 5, 6, 7], d =2 and n = 7
  * A = [1, 2] and B = [3, 4, 5, 6, 7]
  *
---
+++ src/main/java/com/array/FindPairwithSuminSortedRotatedArray.java
@@ -0,0 +1,5 @@
+package com.array;
+
+public class FindPairwithSuminSortedRotatedArray {
+  //https://www.geeksforgeeks.org/given-a-sorted-and-rotated-array-find-if-there-is-a-pair-with-a-given-sum/
+}
