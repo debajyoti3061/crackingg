bcf8423160227bc3a2a8689968bdb27743467e0a
dmahapat <debajyoti.3061@gmail.com>
2019-06-13 14:28:18 -0400

    meeting room

---
+++ src/main/java/com/array/MeetinRoom.java
@@ -0,0 +1,34 @@
+package com.array;
+
+import java.util.*;
+
+public class MeetinRoom {
+
+  public static void main(String[] args){
+    int[][] input={{0,30},{35,45},{20,40}};
+    System.out.println(findsize(input));
+
+  }
+
+  private static int findsize(int[][] input) {
+    Arrays.sort(input, new Comparator<int[]>() {
+      @Override
+      public int compare(int[] o1, int[] o2) {
+        return Integer.compare(o1[0],o2[0]);
+      }
+    });
+
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+    pq.add(input[0][1]);
+    for(int i=1;i<input.length;i++){
+      int start = input[i][0];
+      if (start>=pq.peek()){
+        pq.remove();
+      }
+      pq.add(input[i][1]);
+    }
+
+    return pq.size();
+  }
+}
+
