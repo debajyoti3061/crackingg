42cffc646af4dfccf5ccde3e87484c7a7aceffea
dmahapat <debajyoti.3061@gmail.com>
2019-06-18 10:22:21 -0400

    equqal partition

--- src/main/java/com/dynamic/programming/PartitionEqualSubsetSum.java
+++ src/main/java/com/dynamic/programming/PartitionEqualSubsetSum.java
@@ -1,7 +1,7 @@
 package com.dynamic.programming;
 
 public class PartitionEqualSubsetSum {
-  static boolean findPartition (int arr[], int n)
+ /* static boolean findPartition (int arr[], int n)
   {
     int sum = 0;
     int i, j;
@@ -35,17 +35,37 @@ public class PartitionEqualSubsetSum {
       }
     }
     return part[sum/2][n];
-  }
+  }*/
 
   /*Driver function to check for above function*/
   public static void main (String[] args)
   {
     int arr[] = {3, 1, 1, 2, 2,1};
     int n = arr.length;
-    if (findPartition(arr, n) == true)
+   /* if (findPartition(arr, n) == true)
       System.out.println("Can be divided into two subsets of equal sum");
     else
-      System.out.println("Can not be divided into two subsets of equal sum");
+      System.out.println("Can not be divided into two subsets of equal sum");*/
 
+    System.out.println(canPartition(arr));
+  }
+
+  private static boolean canPartition(int[] arr) {
+    int sum=0;
+    for(int num:arr){
+      sum+=num;
+    }
+    if(sum %2 !=0) return false;
+    sum/=2;
+    boolean[] dp= new boolean[sum+1];
+    dp[0]=true;
+    for(int num:arr){
+      for(int j=sum;j>=0;j--){
+        if(j>=num){
+          dp[j]=dp[j]||dp[j-num];
+        }
+      }
+    }
+    return dp[sum];
   }
 }
