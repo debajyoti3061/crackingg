ae3874780099187518ac115f1b331d7dc120c149
debajyoti3061 <debajyoti.3061@gmail.com>
2019-12-16 00:01:14 -0500

    gas station jump game matrix rotation grouped anagrams

---
+++ src/main/java/com/Strings/GroupedAnagram.java
@@ -0,0 +1,31 @@
+package com.Strings;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupedAnagram {
+    public static void main(String[] args){
+        String input[]={"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> output =groupAnagrams(input);
+    }
+
+    public static List<List<String>> groupAnagrams(String[] input) {
+        List<String> res = new ArrayList<>();
+        // if(strs.length() == 0) return res;
+        HashMap<String,List> map = new HashMap<>();
+        for(String s : input) {
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String key = String.valueOf(c);
+            if(!map.containsKey(key)) {
+                map.put(key,new ArrayList());
+            }
+            map.get(key).add(s);
+
+        }
+        return new ArrayList(map.values());
+    }
+}
+
--- src/main/java/com/dynamic/programming/NumberofwaytoDecode.java
+++ src/main/java/com/dynamic/programming/NumberofwaytoDecode.java
@@ -2,21 +2,26 @@ package com.dynamic.programming;
 //https://www.youtube.com/watch?v=qli-JCrSwuk
 public class NumberofwaytoDecode {
   public static void main(String[] args){
-    System.out.println(numDecoding("123"));
+    System.out.println(numDecoding("0"));
   }
 
-  public static int numDecoding(String s){
-    int[] dp= new int[s.length()+1];
-    dp[0]=1;
-    dp[1]=(s.charAt(0)==0)?0:1;
-    for(int i=2;i<=s.length();i++){
-      int oneDigit= Integer.valueOf(s.substring(i-1,i));
-      int twoDigit= Integer.valueOf(s.substring(i-2,i));
-      if(oneDigit>=1)
-        dp[i]+=dp[i-1];
-      if(twoDigit>=10 && twoDigit<=26)
-        dp[i]+=dp[i-2];
-    }
-    return dp[s.length()];
+  public static int numDecoding(String s){if(s == null || s.length() == 0) {
+    return 0;
   }
-}
+    int n = s.length();
+    int[] dp = new int[n];
+    dp[0] = s.charAt(0) != '0' ? 1 : 0;
+    for(int i = 1; i < n; i++) {
+      int first = Integer.valueOf(s.substring(i, i+1));
+      int second = Integer.valueOf(s.substring(i-1, i+1));
+      if(first >= 1 && first <= 9) {
+        dp[i] += dp[i-1];
+      }
+      if(second >= 10 && second <= 26) {
+        dp[i] += i >=2 ? dp[i-2] : 1;
+      }
+    }
+    return dp[n-1];
+  }
+ }
+
